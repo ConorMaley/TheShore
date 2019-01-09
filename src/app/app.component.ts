665bf52936b41e4681351e5b74e93b247b26c4ee
ConorMaley <cmaley17@wooster.edu>
2019-01-08 20:33:51 -0500

    added JSON import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import * as data from 'assets/data/sample-the-odds-api.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+  	console.log(data);
+  }
 }
